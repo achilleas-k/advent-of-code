26f930902d7c0bf15b0a5c8333a01582863715e0
Achilleas Koutsou <achilleas@koutsou.net>
2022-12-02 18:04:09 +0100

    Solution for 2022/02 in Python

--- 2022/02/02.py
+++ 2022/02/02.py
@@ -34,7 +34,6 @@ rules = {
     }
 }
 
-
 points = {"A": 1, "B": 2, "C": 3}
 
 
@@ -44,16 +43,43 @@ def play_p1(me: str) -> str:
     """
     if me == "X":
         return "A"
+
     if me == "Y":
         return "B"
+
     if me == "Z":
         return "C"
-    raise ValueError(f"Unknown value {me}")
+
+    raise ValueError(f"Unknown play value {me}")
+
+
+def play_p2(opponent: str, outcome: str) -> str:
+    if outcome == "Y":
+        # draw: play the same
+        return opponent
+
+    opponent_idx = "ABC".index(opponent)
+
+    if outcome == "Z":
+        # win: idx+1
+        return "ABC"[(opponent_idx + 1) % 3]
+
+    if outcome == "X":
+        # lose: idx - 1
+        return "ABC"[(opponent_idx - 1) % 3]
+
+    raise ValueError(f"Unknown opponent value {opponent}")
 
 
 def calc_round_score_p1(opponent: str, me: str) -> int:
     # base shape score + outcome score
     me = play_p1(me)
+    return points[me] + rules[opponent][me]
+
+
+def calc_round_score_p2(opponent: str, me: str) -> int:
+    # base shape score + outcome score
+    me = play_p2(opponent, me)
     return points[me] + rules[opponent][me]
 
 
@@ -64,7 +90,19 @@ with open(fname, encoding="utf-8") as infile:
     for idx, line in enumerate(infile):
         opponent, me = line.strip().split(" ", 2)
         round_score = calc_round_score_p1(opponent, me)
+        # print(f"Round {idx+1}: {round_score}")
+        total_score += round_score
+
+
+print(f"Total (part 1): {total_score}")
+
+total_score = 0
+with open(fname, encoding="utf-8") as infile:
+    for idx, line in enumerate(infile):
+        opponent, me = line.strip().split(" ", 2)
+        round_score = calc_round_score_p2(opponent, me)
         print(f"Round {idx+1}: {round_score}")
         total_score += round_score
 
-print(f"Total: {total_score}")
+
+print(f"Total (part 2): {total_score}")

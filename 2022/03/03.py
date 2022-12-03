09e64e164145e901d8406877097c906cbbaedcfd
Achilleas Koutsou <achilleas@koutsou.net>
2022-12-03 16:08:09 +0100

    Solution for 2022/03 in Python

--- 2022/03/03.py
+++ 2022/03/03.py
@@ -1,5 +1,8 @@
 import sys
 
+from typing import List
+
+
 fname = sys.argv[1]
 
 
@@ -37,12 +40,34 @@ def item_priority(item: str) -> int:
     raise ValueError(f"[item_priority] item {item} invalid: must be [a-zA-Z]")
 
 
+def find_group_badge(group_items: List[str]) -> str:
+    """
+    Find the item that represents the 3-elf group: The single common item between all three.
+    """
+    if (length := len(group_items)) != 3:
+        raise ValueError(f"[find_group_badge] group_items has wrong length: {length}")
+
+    common_char = set(group_items[0]).intersection(set(group_items[1])).intersection(set(group_items[2]))
+    if len(common_char) > 1:  # sanity check
+        raise RuntimeError("[find_group_badge] found more than one common item: this shouldn't happen")
+
+    return common_char.pop()
+
+
 print(f"Using data from {fname}")
 
-priority_sums = 0
-with open(fname, encoding="utf-8") as infile:
-    for idx, line in enumerate(infile):
-        priority = item_priority(find_wrong_item(line.strip()))
-        priority_sums += priority
+wrong_item_sum = 0
+group_sum = 0
 
-print(f"Part 1: Sum of priorities of incorrectly packed items: {priority_sums}")
+group = []
+with open(fname, encoding="utf-8") as infile:
+    for line in infile:
+        wrong_item_sum += item_priority(find_wrong_item(line.strip()))
+
+        group.append(line.strip())
+        if len(group) % 3 == 0:  # group every three lines
+            group_sum += item_priority(find_group_badge(group))
+            group = []
+
+print(f"Part 1: Sum of priorities of incorrectly packed items: {wrong_item_sum}")
+print(f"Part 2: Sum of priorities of group badges: {group_sum}")

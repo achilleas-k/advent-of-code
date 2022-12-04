c03e127a1cbfcaf06ebdf1e90da77e91cde26f41
Achilleas Koutsou <achilleas@koutsou.net>
2022-12-04 14:23:10 +0100

    04.py: Call parse_range() from line splitter

--- 2022/04/04.py
+++ 2022/04/04.py
@@ -4,8 +4,10 @@ from typing import Tuple
 
 fname = sys.argv[1]
 
+Range = Tuple[int, int]
 
-def parse_range(rng: str) -> Tuple[int, int]:
+
+def parse_range(rng: str) -> Range:
     elems = rng.split("-")
     if len(elems) != 2:
         raise ValueError(f"[parse_range] bad range string: {rng}")
@@ -14,15 +16,15 @@ def parse_range(rng: str) -> Tuple[int, int]:
     return int(start), int(end)
 
 
-def split_line(line: str) -> Tuple[str, str]:
+def parse_line(line: str) -> Tuple[Range, Range]:
     ranges = line.split(",")
     if len(ranges) != 2:
         raise ValueError(f"[split_line] bad input line: {line}")
 
-    return ranges[0], ranges[1]
+    return parse_range(ranges[0]), parse_range(ranges[1])
 
 
-def superset(a: Tuple[int, int], b: Tuple[int, int]) -> bool:
+def superset(a: Range, b: Range) -> bool:
     if a[0] <= b[0] and a[1] >= b[1]:
         # a fully contains b
         return True
@@ -34,7 +36,7 @@ def superset(a: Tuple[int, int], b: Tuple[int, int]) -> bool:
     return False
 
 
-def overlap(a: Tuple[int, int], b: Tuple[int, int]) -> bool:
+def overlap(a: Range, b: Range) -> bool:
     if superset(a, b):
         # get that out of the way
         return True
@@ -53,9 +55,9 @@ overlaps = 0
 with open(fname, encoding="utf-8") as infile:
     for line in infile:
         line = line.strip()
-        range_a, range_b = split_line(line)
-        supersets += superset(parse_range(range_a), parse_range(range_b))
-        overlaps += overlap(parse_range(range_a), parse_range(range_b))
+        range_a, range_b = parse_line(line)
+        supersets += superset(range_a, range_b)
+        overlaps += overlap(range_a, range_b)
 
 print(f"Supersets: {supersets}")
 print(f"Overlaps:  {overlaps}")

6c7e6ed52453adbbcfff2c50d0ae7c26fac5021c
Achilleas Koutsou <achilleas@koutsou.net>
2022-12-06 14:22:16 +0100

    Solution for 2022/06 in Python
    Both parts included.

---
+++ 2022/06/06.py
@@ -0,0 +1,22 @@
+import sys
+
+
+fname = sys.argv[1]
+
+
+def first_marker_index(line: str, mlen: int) -> int:
+    for idx in range(len(line)):
+        # 4 char sliding window
+        win = line[idx:idx+mlen]
+        if len(set(win)) == len(win):
+            return idx+mlen
+
+    return -1
+
+
+with open(fname, encoding="utf-8") as infile:
+    # the real input is one line long but let's iterate so we can run all the examples from a single file
+    for line in infile:
+        print(f"Part 1: {first_marker_index(line, 4)}")
+        print(f"Part 2: {first_marker_index(line, 14)}")
+        print("--")
